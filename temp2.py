085f3b9b26db73aa81a53e114994dc866ae608fa
Joshua <joshuawallace800@gmail.com>
2017-03-23 12:04:20 -0400

    now generalized things a bit more

--- temp2.py
+++ temp2.py
@@ -21,6 +21,11 @@ from sklearn.model_selection import KFold
 
 import general_functions as general_f
 import data_postprocess as postprocess
+import look_at_results as lar
+
+model_type = "linear"
+imputation_strategy = "most_frequent"
+frac_missing_values_cutoff = 0.85
 
 # Read in the data
 data = general_f.check_if_data_exists_if_not_open_and_read()
@@ -36,10 +41,10 @@ data_to_use = postprocess.convert_NA_values_to_NaN(data_to_use,
                                 also_convert_negatives=True, deepcopy=False)
 
 # Remove columns that have a large fraction of values missing
-data_to_use = postprocess.remove_columns_with_large_number_of_missing_values(data_to_use, 0.85, deepcopy=False)
+data_to_use = postprocess.remove_columns_with_large_number_of_missing_values(data_to_use, frac_missing_values_cutoff, deepcopy=False)
 
 # Impute the NaN values
-imputation = Imputer(missing_values='NaN', strategy='most_frequent')  # mean, median, most_frequent
+imputation = Imputer(missing_values='NaN', strategy=imputation_strategy)  # mean, median, most_frequent
 data_to_use = imputation.fit_transform(data_to_use)
 
 # Convert to Numpy arrays so that the indices can be accessed with a list later
@@ -101,8 +106,5 @@ for val in k_values:
     #Also, save the r_squared_errors across all the folds
     r_squared_error.append(r_squared_error_justthisKvalue)
 
-# print mean_squared_error
-# print r_squared_error
-
-#fig = lar.plot_predict_actual_pairs(prediction, outcomes_in_reserve)
-#fig.savefig("temp2.pdf")
+    fig = lar.plot_predict_actual_pairs(predicted_outcomes, actual_outcomes)
+    fig.savefig("pdf/" + model_type + "_k" + str(val) + "_imp_" + imputation_strategy + "_cutoff" + str(frac_missing_values_cutoff) + ".pdf")
